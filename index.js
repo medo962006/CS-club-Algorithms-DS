1d1548f08341632a04d57fb9e575ea63209e3083
medo962006 <63151191+medo962006@users.noreply.github.com>
2023-11-19 00:13:30 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("Machine Learning JSON file.json");
+const router = jsonServer.router("Algorithms & DS JSON file.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
 
